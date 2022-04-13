793278c7461c63554681c020698563a2ff11f6b8
Lepuroid <lepuroid@gmail.com>
2022-04-13 17:32:49 +0300

    day 1

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,35 @@
 use std::env;
+use regex::Regex;
+use sha2::{Sha256, Digest};
+use hex;
+use std::convert::TryInto;
+
+
+fn vec_to_arr<T, const N: usize>(v: Vec<T>) -> [T; N] {
+    v.try_into()
+        .unwrap_or_else(|v: Vec<T>| panic!("Expected a Vec of length {} but it was {}", N, v.len()))
+}
+
 
 fn main() {
+    let re = Regex::new(r"^[A-F0-9]{64}").unwrap();
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         panic!("PANIC!!!")
     }
-    println!("{}", args[1]);
+    if !re.is_match(&args[1]) {
+        panic!("PANIC!!!")
+    }
+    let hash = vec_to_arr::<u8, 32>(hex::decode(&args[1]).unwrap());
+
+    let mut hasher = Sha256::new();
+    hasher.update(b"hello world");
+    let res = hasher.finalize();
+
+    // arr from generic array
+
+    if hash == res{
+        println!("{:?}", hash);
+        println!("{:?}", res);
+    }
 }
