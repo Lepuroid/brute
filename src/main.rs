b9928e27d9e531b34785b30c275608fdcf8db029
Lepuroid <lepuroid@gmail.com>
2022-04-15 16:24:23 +0300

    day 3

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,13 @@ use hex;
 use std::convert::TryInto;
 
 
-fn vec_to_arr<T, const N: usize>(v: Vec<T>) -> [T; N] {
-    v.try_into()
-        .unwrap_or_else(|v: Vec<T>| panic!("Expected a Vec of length {} but it was {}", N, v.len()))
+fn inc_char(mut c: char) -> char {
+    if c < '~' {
+        c = ((c as u8) + 1) as char;
+    } else {
+        c = '!';
+    }
+    c
 }
 
 
@@ -17,11 +21,20 @@ fn main() {
     if args.len() < 2 || !re.is_match(&args[1]){
         panic!("PANIC!!!")
     }
-    let hash = vec_to_arr::<u8, 32>(hex::decode(&args[1]).unwrap());
-    let samp = Sha256::digest(b"hello world");
-    if hash == <[u8; 32]>::from(samp) {
-        println!("{:?}", hash);
-        println!("{:?}", samp);
+    let hash: [u8; 32] = hex::decode(&args[1]).unwrap().as_slice().try_into().unwrap();
+    let mut c = '!';
+    let mut s = String::from(c);
+    let mut samp = <[u8; 32]>::from(Sha256::digest(&s));
+
+    while !(hash == samp){
+        c = inc_char(c);
+        s.pop();
+        s.push(c);
+        samp = <[u8; 32]>::from(Sha256::digest(&s));
+        if c == '~' {
+            s.push('!');
+        }
+        println!("{}", s);
     }
     println!("{}", "The End!");
 }
